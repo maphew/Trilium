54c0bef7800747ecb2e67c3fec7373bf5788ebcd
Anand Hegde <anandghegde@gmail.com>
2026-09-14 21:00:24 +0530

    docs(desktop): shorten Icon Composer leaf note in create-icons.sh

--- scripts/icons/create-icons.sh
+++ scripts/icons/create-icons.sh
@@ -71,10 +71,8 @@ done
 npx iconsur set fakeapp.app -l -i "png/1024x1024-dev.png" -o "mac/master-dev.png" -s 0.8
 build_icns "mac/master-dev.png" "icon-dev.icns" "mac/dev"
 
-# icon.icon and icon-dev.icon (Icon Composer documents for macOS 26) are NOT generated here. Their
-# Assets/leaf-{right,bottom,left}.svg are the three path groups of icon-color.svg and
-# icon-purple.svg, one leaf per file on the same 256x256 viewBox, with icon-color.svg's CSS classes
-# inlined as fill attributes. After changing either source SVG, update those leaf files by hand.
+# The Icon Composer leaves in icon.icon/Assets and icon-dev.icon/Assets are hand-copied path groups of
+# icon-color.svg and icon-purple.svg, so edit them whenever those source SVGs change.
 
 # Build Windows icon
 magick -background none "$source_icon_dir/icon-color.svg" -define icon:auto-resize=16,32,48,64,128,256 "./icon.ico"
